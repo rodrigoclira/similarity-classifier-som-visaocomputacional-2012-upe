1aeea9c30fe1bdf3b72b3014599b1842a07f9e07
rodrigocliras <rodrigocliras@55a7dfbb-8c75-26ba-a3a9-4a27e8d0c843>
2012-10-20 13:09:46 +0000

    Inserção do novo precision

--- trunk/src/Simulation/ClassificatorTest.java
+++ trunk/src/Simulation/ClassificatorTest.java
@@ -1,11 +1,13 @@
 package Simulation;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
 
 import Classification.Classificator;
 import Classification.Image;
 import Util.ImageXMLReader;
+import Util.PrecisionRecall;
 
 public class ClassificatorTest {
 
@@ -19,7 +21,9 @@ public class ClassificatorTest {
 			ImageXMLReader xml = new ImageXMLReader("src//xml//images.xml");
 			xml.normalizarArray(0.1, 0.9);
 			ArrayList<Image> dataBase = xml.getImages();
-			
+			Double precision[] = new Double[191];
+			Double recall[] = new Double[191];
+			PrecisionRecall pr = new PrecisionRecall();
 			double t0 = System.currentTimeMillis();
 			for (Image image : dataBase) {
 				
@@ -35,13 +39,16 @@ public class ClassificatorTest {
 				//Collections.shuffle(partialImageSet);
 				ArrayList<Image> result = somClassificator.classify(image, partialImageSet);
 				
+				pr.run(5, 960, image, result);
 				//fazer calculo do precision recall
 				//salvar resultados
 				
-				for (Image test : result) {
-					System.out.println("ID: " + test.getId() + " Label: "+ test.getLabel() + " distance: " + test.getDistanceBySearchImage());
-				}				
+//				for (Image test : result) {
+//					System.out.println("ID: " + test.getId() + " Label: "+ test.getLabel() + " distance: " + test.getDistanceBySearchImage());
+//				}				
 			}					
+			
+			pr.saveResult("src" + File.separator + "Results" + File.separator + "precisionRecall.txt");
 			
 			System.out.println("Tempo: " + (System.currentTimeMillis() - t0));
 
